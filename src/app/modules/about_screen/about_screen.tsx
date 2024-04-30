449cac0555be99b141b9fab7ec0a2cef84b0e06b
Алексей А. Кузнецов <alexsmith_proff@mail.ru>
2024-04-30 11:18:11 +0300

    111

--- src/app/modules/about_screen/about_screen.tsx
+++ src/app/modules/about_screen/about_screen.tsx
@@ -9,7 +9,7 @@ const AboutScreen: FC = ({ }) => {
         <div className={s.main}>
             <div className="container">
                 <div className={s.content}>
-                    <div className={s.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit aliquid voluptate laudantium, aspernatur culpa provident ad at quisquam nemo ratione quas temporibus quia, optio beatae delectus libero magnam ducimus est dignissimos labore? Dolorum nesciunt ex, aut quam laborum iure temporibus aliquam fuga itaque! Necessitatibus alias facere odio illum suscipit aspernatur officiis maiores culpa quis sequi. Distinctio voluptatibus nemo, quo explicabo quasi exercitationem minus rerum tempore debitis animi. Eum deserunt esse modi repellendus nostrum numquam ad perferendis quod expedita fuga?</div>
+                    <div className={s.text}>Lorem ipsum dolor sit amet adipisicing elit. Ut suscipit aliquid voluptate laudantium, aspernatur culpa provident ad at quisquam nemo ratione quas temporibus quia, optio beatae delectus libero magnam ducimus est dignissimos labore? Dolorum nesciunt ex, aut quam laborum iure temporibus aliquam fuga itaque! Necessitatibus alias facere odio illum suscipit aspernatur officiis maiores culpa quis sequi. Distinctio voluptatibus nemo, quo explicabo quasi exercitationem minus rerum tempore debitis animi. Eum deserunt esse modi repellendus nostrum numquam ad perferendis quod expedita fuga?</div>
                     <ul className={s.photoList}>
                         <li className={`${s.photoItem} ${s.photo1}`}>
                             <Image className={s.img} src={'/img/about/1.jpg'} width={405} height={400} alt="about-photo1" />
