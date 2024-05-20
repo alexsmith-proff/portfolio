0be29bb0e0dcef55f2993e91a65cc89c04fa5634
Алексей А. Кузнецов <alexsmith_proff@mail.ru>
2024-05-20 14:01:32 +0300

    add

---
+++ src/app/project/[id]/page.jsx
@@ -0,0 +1,11 @@
+
+import MainProjects from "@/app/modules/main_projects/main_projects";
+
+export default function ProjectPage({ params }) {
+  return (
+    <main>
+      <MainProjects title="Другие проекты" />
+      
+    </main>
+  );
+}
