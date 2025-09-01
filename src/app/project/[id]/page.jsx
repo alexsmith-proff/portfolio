7f686bacb8d7073d2fdd9f9e4f394ee1af4b5d26
Алексей А. Кузнецов <alexsmith_proff@mail.ru>
2025-09-01 14:44:13 +0300

    fix

--- src/app/project/[id]/page.jsx
+++ src/app/project/[id]/page.jsx
@@ -4,6 +4,10 @@ import { projects } from "@/app/constants/projects";
 import MainProjects from "@/app/modules/main_projects/main_projects";
 import ProjectScreen from "@/app/modules/project_screen/project_screen";
 
+export async function generateStaticParams() {
+  return projects.map(project => ({ id: String(project.id) }));
+}
+
 export default function ProjectPage({ params }) {
   return (
     <main>
